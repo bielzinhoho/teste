e15d2452b329816dcee9f745dcfa1dea874c9142
unknown <gabrielpereiracoelho18@gmail.com>
2024-06-20 20:34:33 -0300

    primeiro commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+// Exemplo de script para futuras funcionalidades
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Documento carregado e pronto.');
+    // Adicione funcionalidades JavaScript aqui
+});
